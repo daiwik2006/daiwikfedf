c1d4caf00871a197b5fcf7dbfc9697728a18be53
daiwik2006 <daiwiknaidugorle2006@gmail.com>
2025-07-21 15:27:56 +0530

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import App from './App';
+import Myapp from './Myapp';
+import Welcome from './Welcome';
 import './index.css'
-import App from './App.jsx'
-import Myapp from './Myapp.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Welcome from './Welcome.jsx'
 
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename='p1'>
     <Routes>
-      <Route path="app" element={<App/>}></Route>
-      <Route path='myapp' element={<Myapp/>}></Route>
-      <Route path='/' element={<Welcome/>}></Route>
+      <Route path="/" element={<Welcome />} />
+      <Route path="app" element={<App />} />
+      <Route path="myapp" element={<Myapp />} />
     </Routes>
   </BrowserRouter>
-)
+);
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 2006}
+  server: {port:2006},
+  base: '/p1'
 })
